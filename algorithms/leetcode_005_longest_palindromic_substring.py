cf874e3958cff1fe5959144a1f9665e9a07ad498
NingAnMe <ninganme@qq.com>
2018-02-07 16:47:02 +0800

    增加时间测试

--- algorithms/leetcode_005_longest_palindromic_substring.py
+++ algorithms/leetcode_005_longest_palindromic_substring.py
@@ -26,89 +26,100 @@ Output: "c"
 """
 
 
-def longestPalindrome(s):
-    """
-    寻找一个字符串的最长回文
-    :param s:
-    :return:
-    """
-    n = len(s)
-    if n == 0:
-        return None
-    if n == 1:
-        return s
-    elif isPalindrome(s):
-        return s
-    else:
-        longest = 0
-        longest_sub = None
-        for i in xrange(0, n):
-            sub_s = s[:-i]
-            if isPalindrome(sub_s):
-                length = len(sub_s)
-                if length > longest:
-                    longest = length
-                    longest_sub = sub_s
-                break
-            else:
-                sub_s = s[i:]
-                if isPalindrome(sub_s):
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        """
+        寻找一个字符串的最长回文
+        :param s:
+        :return:
+        """
+        n = len(s)
+        if n == 0:
+            return None
+        if n == 1:
+            return s
+        elif self.isPalindrome(s):
+            return s
+        else:
+            longest = 0
+            longest_sub = None
+            for i in xrange(0, n):
+                sub_s = s[:-i]
+                if self.isPalindrome(sub_s):
                     length = len(sub_s)
                     if length > longest:
                         longest = length
                         longest_sub = sub_s
                     break
+                else:
+                    sub_s = s[i:]
+                    if self.isPalindrome(sub_s):
+                        length = len(sub_s)
+                        if length > longest:
+                            longest = length
+                            longest_sub = sub_s
+                        break
 
-    if longest >= (n - 2):
-        return longest_sub
-    else:
-        sub = s[1: -1]
-        longest_s = longestPalindrome(sub)
-        if longest_s is not None and len(longest_s) > longest:
-            return longest_s
-        else:
+        if longest >= (n - 2):
             return longest_sub
-
-
-def isPalindrome(s):
-    """
-    判断是否是回文
-    :param s:
-    :return:
-    """
-    length = len(s)
-    if length == 0:
-        return False
-    n = length / 2
-    for i in xrange(0, n):
-        start = i
-        end = (i + 1) * (-1)
-        if s[start] == s[end]:
-            continue
         else:
+            sub = s[1: -1]
+            longest_s = self.longestPalindrome(sub)
+            if longest_s is not None and len(longest_s) > longest:
+                return longest_s
+            else:
+                return longest_sub
+
+    def isPalindrome(self, s):
+        """
+        判断是否是回文
+        :param s:
+        :return:
+        """
+        length = len(s)
+        if length == 0:
             return False
-    return True
+        n = length / 2
+        for i in xrange(0, n):
+            start = i
+            end = (i + 1) * (-1)
+            if s[start] == s[end]:
+                continue
+            else:
+                return False
+        return True
 
 
 if __name__ == '__main__':
-    s = ''
-    print(isPalindrome(s))
-    s = 'a'
-    print(isPalindrome(s))
-    s = 'ab'
-    print(isPalindrome(s))
-    s = 'bb'
-    print(isPalindrome(s))
-    s = 'aba'
-    print(isPalindrome(s))
+    import time
+
+    solution = Solution()
 
     s = ''
-    print(longestPalindrome(s))
+    print(solution.isPalindrome(s))
     s = 'a'
-    print(longestPalindrome(s))
+    print(solution.isPalindrome(s))
     s = 'ab'
-    print(longestPalindrome(s))
+    print(solution.isPalindrome(s))
     s = 'bb'
-    print(longestPalindrome(s))
+    print(solution.isPalindrome(s))
     s = 'aba'
-    print(longestPalindrome(s))
+    print(solution.isPalindrome(s))
+
+    s = ''
+    print(solution.longestPalindrome(s))
+    s = 'a'
+    print(solution.longestPalindrome(s))
+    s = 'ab'
+    print(solution.longestPalindrome(s))
+    s = 'bb'
+    print(solution.longestPalindrome(s))
+    s = 'aba'
+    print(solution.longestPalindrome(s))
+
+    ss = "vaomxdtiuwqlwhgutkhxxhccsgvyoaccuicgybnqnslogtqhblegfudagpxfvjdacsxgevvepuwthdtybgflsxjdmmfumyqgpxatvdypjmlapccaxwkuxkilqqgpihyepkilhlfkdrbsefinitdcaghqmhylnixidrygdnzmgubeybczjceiybowglkywrpkfcwpsjbkcpnvfbxnpuqzhotzspgebptnhwevbkcueyzecdrjpbpxemagnwmtwikmkpqluwmvyswvxghajknjxfazshsvjkstkezdlbnkwxawlwkqnxghjzyigkvqpapvsntojnxlmtywdrommoltpbvxwqyijpkirvndwpgufgjelqvwffpuycqfwenhzrbzbdtupyutgccdjyvhptnuhxdwbmdcbpfvxvtfryszhaakwshrjseonfvjrrdefyxefqfvadlwmedpvnozobftnnsutegrtxhwitrwdpfienhdbvvykoynrsbpmzjtotjxbvemgoxreiveakmmbbvbmfbbnyfxwrueswdlxvuelbkrdxlutyukppkzjnmfmclqpkwzyylwlzsvriwomchzzqwqglpflaepoxcnnewzstvegyaowwhgvcwjhbbstvzhhvghigoazbjiikglbqlxlccrwqvyqxpbtpoqjliziwmdkzfsrqtqdkeniulsavsfqsjwnvpprvczcujihoqeanobhlsvbzmgflhykndfydbxatskf "
+    start = time.clock()
+    p = solution.longestPalindrome(ss)
+    end = time.clock()
+    print end - start
