0469c4107fec57f17101c609e9048092541e5446
NingAnMe <ninganme@qq.com>
2018-02-07 15:39:09 +0800

    精简

--- algorithms/leetcode_005_longest_palindromic_substring.py
+++ algorithms/leetcode_005_longest_palindromic_substring.py
@@ -26,7 +26,7 @@ Output: "c"
 """
 
 
-def longest_palindrome(s):
+def longestPalindrome(s):
     """
     寻找一个字符串的最长回文
     :param s:
@@ -37,43 +37,40 @@ def longest_palindrome(s):
         return None
     if n == 1:
         return s
-    elif is_palindrome(s):
+    elif isPalindrome(s):
         return s
     else:
         longest = 0
         longest_sub = None
         for i in xrange(0, n):
             sub_s = s[:-i]
-            if not is_palindrome(sub_s):
-                continue
-            else:
+            if isPalindrome(sub_s):
                 length = len(sub_s)
                 if length > longest:
                     longest = length
                     longest_sub = sub_s
                 break
-        for i in xrange(0, n):
-            sub_s = s[i:]
-            if not is_palindrome(sub_s):
-                continue
             else:
-                length = len(sub_s)
-                if length > longest:
-                    longest = length
-                    longest_sub = sub_s
-                break
-        if longest >= (n - 2):
-            return longest_sub
+                sub_s = s[i:]
+                if isPalindrome(sub_s):
+                    length = len(sub_s)
+                    if length > longest:
+                        longest = length
+                        longest_sub = sub_s
+                    break
+
+    if longest >= (n - 2):
+        return longest_sub
+    else:
+        sub = s[1: -1]
+        longest_s = longestPalindrome(sub)
+        if longest_s is not None and len(longest_s) > longest:
+            return longest_s
         else:
-            sub = s[1: -1]
-            longest_s = longest_palindrome(sub)
-            if longest_s is not None and len(longest_s) > longest:
-                return longest_s
-            else:
-                return longest_sub
+            return longest_sub
 
 
-def is_palindrome(s):
+def isPalindrome(s):
     """
     判断是否是回文
     :param s:
@@ -95,23 +92,23 @@ def is_palindrome(s):
 
 if __name__ == '__main__':
     s = ''
-    print(is_palindrome(s))
+    print(isPalindrome(s))
     s = 'a'
-    print(is_palindrome(s))
+    print(isPalindrome(s))
     s = 'ab'
-    print(is_palindrome(s))
+    print(isPalindrome(s))
     s = 'bb'
-    print(is_palindrome(s))
+    print(isPalindrome(s))
     s = 'aba'
-    print(is_palindrome(s))
+    print(isPalindrome(s))
 
     s = ''
-    print(longest_palindrome(s))
+    print(longestPalindrome(s))
     s = 'a'
-    print(longest_palindrome(s))
+    print(longestPalindrome(s))
     s = 'ab'
-    print(longest_palindrome(s))
+    print(longestPalindrome(s))
     s = 'bb'
-    print(longest_palindrome(s))
+    print(longestPalindrome(s))
     s = 'aba'
-    print(longest_palindrome(s))
+    print(longestPalindrome(s))
