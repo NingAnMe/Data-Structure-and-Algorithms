732e9296e6e244daf707fe21abb6e578f4ea8e16
NingAnMe <ninganme@qq.com>
2018-08-22 17:37:32 +0800

    添加算法描述

--- Python/algorithms/removeDuplicates.py
+++ Python/algorithms/removeDuplicates.py
@@ -4,8 +4,7 @@
 @Time    : 2018/6/14 16:21
 @Author  : AnNing
 """
-"""
-  从排序数组中删除重复项
+"""删除排序数组中的重复项
 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
@@ -42,6 +41,15 @@ for (int i = 0; i < len; i++) {
 }
 """
 
+"""算法
+1. 记录需要被修改的位置的索引 index_modify，初始值为 0
+2. 记录当前移动到的位置的索引 index_now，初始值为 0
+3. 记录当前的最大值 num_max，初始值为 -1（nums 的取值范围 > 0)
+4. 从第一个数字开始循环处理，
+   当 num_now 大于 num_max 的时候，修改 index_modify 处的数字为 num_now, index_modify + 1
+5. 新数组的长度等于循环结束时 index_modify 的值
+
+"""
 
 class Solution(object):
     def removeDuplicates(self, nums):
