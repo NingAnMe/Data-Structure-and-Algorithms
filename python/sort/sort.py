d14808881153b3dfa396994546b486a6803deeea
NingAnMe <ninganme@qq.com>
2019-03-05 14:26:50 +0800

    添加sort模板

--- python/sort/sort.py
+++ python/sort/sort.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-@Time    : 2019/3/4
-@Author  : AnNing
-"""
+
+
+class Sort:
+    @classmethod
+    def sort(cls, a: list):
+        pass
+
+    @classmethod
+    def less(cls, v: object, w: object):
+        return v < w
+
+    @classmethod
+    def exch(cls, a: list, i: int, j: int):
+        a[i], a[j] = a[j], a[i]
+
+    @classmethod
+    def show(cls, a: list):
+        print(a)
+
+    @classmethod
+    def is_sorted(cls, a: list):
+        length = len(a)
+        for i in range(1, length):
+            if cls.less(a[i], a[i-1]):
+                return False
+        return True
+
