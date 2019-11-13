20bade52cad655d656ba9c129c91d2eccaf0f9b0
NingAnMe <ninganme@qq.com>
2019-11-13 10:24:58 +0800

    杨辉三角的算法

---
+++ algorithms/quartile.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/13 10:02
+# @Author  : NingAnMe <ninganme@qq.com>
---
+++ algorithms/yang_hui_san_jiao.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/12 10:28
+# @Author  : NingAnMe <ninganme@qq.com>
+
+n = int(input('请输入三角形高度：'))
+
+layer = 1
+values = [1]
+
+while layer <= n:
+    new_values = [1]
+
+    index = 0
+    while index < len(values):
+        print('%d ' % values[index], end='')
+        if index < len(values) - 1:
+            new_values.append(values[index] + values[index + 1])
+        index += 1
+    new_values.append(1)
+    values = new_values
+    print('')
+    layer += 1
